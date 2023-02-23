67ed525c9330516b1fc1e743f95283564eb83238
lanemunds <lanemunds@gmail.com>
2023-02-23 12:19:05 -0700

    pretty table

--- another_module.py
+++ another_module.py
@@ -1,1 +1,6 @@
-another_variable = 9
+from prettytable import PrettyTable
+
+table = PrettyTable()
+table.add_column('Pokemon Name',['Pikachu','Squirtle','Charmander'])
+table.add_column('Type',['Electric','Water','Fire'])
+print(table)
