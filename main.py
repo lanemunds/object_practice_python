647becb460abdc22771f557a2bec18379d972863
lanemunds <lanemunds@gmail.com>
2023-02-23 12:04:31 -0700

    turtle

---
+++ main.py
@@ -0,0 +1,19 @@
+import another_module
+
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+print(timmy)
+timmy.shape('turtle')
+timmy.color('DarkGreen')
+timmy.forward(100)
+timmy.right(90)
+timmy.forward(100)
+timmy.right(90)
+timmy.forward(100)
+timmy.right(90)
+timmy.forward(100)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
